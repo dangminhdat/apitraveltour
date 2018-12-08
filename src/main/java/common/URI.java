f5a3ec4d5f44e139f9f3769f8efca64d1f9b78b0
Đạt Đặng <dangminhdat.qnam@gmail.com>
2018-12-09 00:21:47 +0700

    Fix gui.

--- src/main/java/common/URI.java
+++ src/main/java/common/URI.java
@@ -9,40 +9,40 @@ package common;
 
 public class URI {
 
-    public class API {
+	public class API {
 
-        // API control user
-        public static final String URI_USERS = "https://apitour.herokuapp.com/api/users";
+		// API control user
+		public static final String URI_USERS = "https://apitour.herokuapp.com/api/users";
 
-        // API get profile user
-        public static final String URI_PROFILE = "https://apitour.herokuapp.com/api/profile";
+		// API get profile user
+		public static final String URI_PROFILE = "https://apitour.herokuapp.com/api/profile";
 
-        // API control hotel
-        public static final String URI_HOTELS = "https://apitour.herokuapp.com/api/hotels";
+		// API control hotel
+		public static final String URI_HOTELS = "https://apitour.herokuapp.com/api/hotels";
 
-        // API control guide
-        public static final String URI_GUIDES = "https://apitour.herokuapp.com/api/guides";
+		// API control guide
+		public static final String URI_GUIDES = "https://apitour.herokuapp.com/api/guides";
 
-        // API control tour
-        public static final String URI_TOUR = "https://apitour.herokuapp.com/api/tour";
-        
-        // API get tour
-        public static final String URI_GET_TOUR = "https://apitour.herokuapp.com/api/get-tour";
+		// API control tour
+		public static final String URI_TOUR = "https://apitour.herokuapp.com/api/tour";
 
-        // API get tour
-        public static final String URI_GET_DETAIL_TOUR_BY_ID = "https://apitour.herokuapp.com/api/get-tour";
+		// API get tour
+		public static final String URI_GET_TOUR = "https://apitour.herokuapp.com/api/get-tour";
 
-        // API control detail tour
-        public static final String URI_DETAIL_TOUR = "https://apitour.herokuapp.com/api/detail";
+		// API get tour
+		public static final String URI_GET_DETAIL_TOUR_BY_ID = "https://apitour.herokuapp.com/api/get-tour";
 
-        // API upload images
-        public static final String URI_UPLOAD_IMG = "https://apitour.herokuapp.com/api/upload";
+		// API control detail tour
+		public static final String URI_DETAIL_TOUR = "https://apitour.herokuapp.com/api/detail";
 
-        // API control review
-        public static final String URI_REVIEW = "https://apitour.herokuapp.com/api/review";
+		// API upload images
+		public static final String URI_UPLOAD_IMG = "https://apitour.herokuapp.com/api/upload";
 
-        // API logout
-        public static final String URI_LOGOUT = "https://apitour.herokuapp.com/api/logout";
+		// API control review
+		public static final String URI_REVIEW = "https://apitour.herokuapp.com/api/review";
 
-    }
+		// API logout
+		public static final String URI_LOGOUT = "https://apitour.herokuapp.com/api/logout";
+
+	}
 }
